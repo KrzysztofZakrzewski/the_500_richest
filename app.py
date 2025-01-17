b89937fc7ad16cf7e823e314fb4fcc7933bd90b4
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2025-01-17 04:54:18 +0100

    fix2

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+import statsmodels.api as sm
 import io
 
 pd.options.display.float_format = '{:,.2f}'.format
